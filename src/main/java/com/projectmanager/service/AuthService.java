1122233f4a942501665abf26636134472d15e3b3
Tăng Huỳnh Tuấn Tú <tanghuynhtuantuwork@gmail.com>
2026-07-05 21:02:09 +0700

    feat: update AuthService login logic

--- src/main/java/com/projectmanager/service/AuthService.java
+++ src/main/java/com/projectmanager/service/AuthService.java
@@ -26,5 +26,28 @@ public class AuthService {
     }
 
     //register: trả về user nếu đăng ký thành công, trả về null nếu thất bại
-    public boolean register()
+    public boolean register(String username, String password, String confirmPassword, String email, String role){
+        
+        Validator.requireNonBlank(username, "Username");
+        Validator.requireMinLength(username, "Username",3);
+        Validator.requireNonBlank(password, "Mat khau");
+        Validator.requireMinLength(password, "Mat khau", 6);
+
+        if(!password.equals(confirmPassword)){
+            throw new IllegalArgumentException("Mat khau xac nhan khong khop. ");
+        }
+
+        if(userRepo.existByUsername(username)){
+            throw new IllegalArgumentException("Username \"" + username + "\" da ton tai. ");
+        }
+
+        User u = new User();
+        u.username = username;
+        u.passwordHash = PasswordHasher.hash(password);
+        u.email = (email == null) ? "" : email.trim();
+        u.role = (role == null || role.isBlank())? "user" : role;
+        u.status = true;
+
+        return userRepo.insert(u);
+    }
 }
